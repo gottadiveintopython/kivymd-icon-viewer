672a6471c919215cac035c41590d028c99165962
Nattōsai Mitō <flow4re2c@gmail.com>
2021-11-03 19:59:04 +0900

    remove reduntant code

--- src/main.py
+++ src/main.py
@@ -1,19 +1,24 @@
+def immediate_call(func):
+    return func()
+
+
+@immediate_call
 def kivymd_copy_dir():
     from pathlib import PurePath
     return PurePath(__file__).parent / 'kivymd_copy'
-kivymd_copy_dir = kivymd_copy_dir()
 
 
-def _use_a_local_copy_of_kivymd_if_the_official_one_is_not_installed():
+@immediate_call
+def _use_the_local_copy_of_kivymd_if_the_official_one_is_not_installed():
     try:
         import kivymd
     except ImportError:
         import sys
         sys.path.append(str(kivymd_copy_dir))
-        import kivymd
-_use_a_local_copy_of_kivymd_if_the_official_one_is_not_installed()
+    import kivymd
 
 
+@immediate_call
 def _register_icon_font_if_its_not():
     try:
         import kivymd.font_definitions
@@ -27,7 +32,6 @@ def _register_icon_font_if_its_not():
                 'kivymd',
                 'fonts',
                 'materialdesignicons-webfont.ttf')))
-_register_icon_font_if_its_not()
 
 
 from kivy.app import App
